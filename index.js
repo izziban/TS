c130a57540218a7bf053d890866fc73546a4a92c
izziban <izziban1@gmail.com>
2023-06-09 12:16:41 +0900

    second commit

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-var 이름 = 'kim';
+var 이름 = 123;
+var 이름2 = '홍길동';
+function 함수(x) {
+    return x * 2;
+}
--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 type 내타입 = string | number
 
 let 이름:내타입 = 123;
+let 이름2:내타입 = '홍길동';
 
 function 함수(x : number) : number {
     return x * 2
